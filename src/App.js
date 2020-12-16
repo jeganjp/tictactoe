d657dd70998277bef6e0dc959bfaa2e93d837e11
jeganjp <jeganjp1999@gmail.com>
2020-12-16 23:24:56 +0530

    init, board , square , scss

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Board from "./Components/Board";
+import "./styles/root.scss"
+
+export default () => (
+  <div className='app'>
+    <h1>TIC TAC TOE</h1>
+    <Board />
+  </div>
+);
